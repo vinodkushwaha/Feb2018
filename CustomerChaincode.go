3b4c5919175e99dcf6d26c31656ee810c323a9ef
gulalkumar <gulalkumar@gmail.com>
2017-02-14 18:13:10 +0530

    Updated ChainCode

--- CustomerChaincode.go
+++ CustomerChaincode.go
@@ -5,12 +5,13 @@ Copyright Capgemini India. 2016 All Rights Reserved.
 package main
 
 import (
-	"errors"
+
 	"fmt"
 	//"strconv"
 	"encoding/json"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	//"github.com/golang/protobuf/ptypes/timestamp"
+	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
 // Customer Chaincode implementation
@@ -28,40 +29,42 @@ type CustomerData struct{
 	}
 
 
-func (t *CustomerChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *CustomerChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 
 	var err error
 	// Initialize the chaincode
-
+	// function, args := stub.GetFunctionAndParameters()
 	fmt.Printf("Deployment of Customer ChainCode is completed\n")
 
 	var emptyCustomerTxs []CustomerData
 	jsonAsBytes, _ := json.Marshal(emptyCustomerTxs)
 	err = stub.PutState(customerIndexTxStr, jsonAsBytes)
 	if err != nil {
-		return nil, err
+		return shim.Error(err.Error())
 	}
 
 
-	return nil, nil
+	return shim.Success(nil)
 }
 
 // Add customer data for the policy
-func (t *CustomerChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if function == customerIndexTxStr {
+func (t *CustomerChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response{
+	function, args := stub.GetFunctionAndParameters()
+	if function == "invoke" {
 		return t.RegisterCustomer(stub, args)
 	}
-	return nil, nil
+	return shim.Error("Invalid invoke function name. Expecting \"invoke\" \"delete\" \"query\"")
+
 }
 
-func (t *CustomerChaincode)  RegisterCustomer(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+func (t *CustomerChaincode)  RegisterCustomer(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
 	var CustomerDataObj CustomerData
 	var CustomerDataList []CustomerData
 	var err error
 
 	if len(args) != 5 {
-		return nil, errors.New("Incorrect number of arguments. Need 5 arguments")
+		return shim.Error("Incorrect number of arguments. Need 5 arguments")
 	}
 
 	// Initialize the chaincode
@@ -75,7 +78,7 @@ func (t *CustomerChaincode)  RegisterCustomer(stub shim.ChaincodeStubInterface, 
 
 	customerTxsAsBytes, err := stub.GetState(customerIndexTxStr)
 	if err != nil {
-		return nil, errors.New("Failed to get customer transactions")
+		return shim.Error("Failed to get customer transactions")
 	}
 	json.Unmarshal(customerTxsAsBytes, &CustomerDataList)
 
@@ -84,13 +87,13 @@ func (t *CustomerChaincode)  RegisterCustomer(stub shim.ChaincodeStubInterface, 
 
 	err = stub.PutState(customerIndexTxStr, jsonAsBytes)
 	if err != nil {
-		return nil, err
+		return shim.Error(err.Error())
 	}
-	return nil, nil
+	return shim.Success(nil)
 }
 
 // Query callback representing the query of a chaincode
-func (t *CustomerChaincode) Query(stub shim.ChaincodeStubInterface,function string, args []string) ([]byte, error) {
+func (t *CustomerChaincode) Query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
 	var customer_name string // Entities
 	var customer_id string
@@ -99,31 +102,32 @@ func (t *CustomerChaincode) Query(stub shim.ChaincodeStubInterface,function stri
 	var resAsBytes []byte
 
 	if len(args) != 3 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 3 parameters to query")
+		return shim.Error("Incorrect number of arguments. Expecting 3 parameters to query")
 	}
 
 	customer_name = args[0]
 	customer_id = args[1]
 	customer_dob = args[2]
 
-	resAsBytes, err = t.GetCustomerDetails(stub, customer_name, customer_id, customer_dob)
+	return (t.GetCustomerDetails(stub, customer_name, customer_id, customer_dob))
 
 	fmt.Printf("Query Response:%s\n", resAsBytes)
 
 	if err != nil {
-		return nil, err
+		jsonResp := "{\"Error\":\"Failed to get state for " + customer_name + "\"}"
+		return shim.Error(jsonResp)
 	}
 
-	return resAsBytes, nil
+	return shim.Success(resAsBytes)
 }
 
-func (t *CustomerChaincode)  GetCustomerDetails(stub shim.ChaincodeStubInterface, customer_name string, customer_id string, customer_dob string) ([]byte, error) {
+func (t *CustomerChaincode)  GetCustomerDetails(stub shim.ChaincodeStubInterface, customer_name string, customer_id string, customer_dob string) pb.Response {
 
 	//var requiredObj CustomerData
 	var objFound bool
 	CustomerTxsAsBytes, err := stub.GetState(customerIndexTxStr)
 	if err != nil {
-		return nil, errors.New("Failed to get Merchant Transactions")
+		return shim.Error("Failed to get Customer Transactions")
 	}
 	var CustomerTxObjects []CustomerData
 	var CustomerTxObjects1 []CustomerData
@@ -134,9 +138,9 @@ func (t *CustomerChaincode)  GetCustomerDetails(stub shim.ChaincodeStubInterface
 	if customer_id == "" {
 		res, err := json.Marshal(CustomerTxObjects)
 		if err != nil {
-		return nil, errors.New("Failed to Marshal the required Obj")
+		return shim.Error("Failed to Marshal the required Obj")
 		}
-		return res, nil
+		return shim.Success(res)
 	}
 
 	objFound = false
@@ -153,20 +157,20 @@ func (t *CustomerChaincode)  GetCustomerDetails(stub shim.ChaincodeStubInterface
 	if objFound {
 		res, err := json.Marshal(CustomerTxObjects1)
 		if err != nil {
-		return nil, errors.New("Failed to Marshal the required Obj")
+		return shim.Error("Failed to Marshal the required Obj")
 		}
-		return res, nil
+		return shim.Success(res)
 	} else {
 		res, err := json.Marshal("No Data found")
 		if err != nil {
-		return nil, errors.New("Failed to Marshal the required Obj")
+		return shim.Error("Failed to Marshal the required Obj")
 		}
-		return res, nil
+		return shim.Success(res)
 	}
 }
 
 func main() {
-	err := shim.Start(new(SimpleChaincode))
+	err := shim.Start(new(CustomerChaincode))
 	if err != nil {
 		fmt.Printf("Error starting Customer Simple chaincode: %s", err)
 	}
