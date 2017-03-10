6035fa13e137d15f16d95adc4a12b10738eb72dd
vinodkushwaha <er.tinku24@rediffmail.com>
2017-03-10 14:44:49 +0530

    Update CustomerChaincode.go

--- CustomerChaincode.go
+++ CustomerChaincode.go
@@ -99,9 +99,9 @@ func (t *CustomerChaincode) Invoke(stub shim.ChaincodeStubInterface, function st
     fmt.Printf("********Invoke Call with args length :%s\n", len(args))
 	if len(args) < 31 {
 	    fmt.Printf("********Inside Invoke length:%s\n", len(args))
-		return -1, errors.New("Incorrect number of arguments. Need 31 arguments")
+		return error, errors.New("Incorrect number of arguments. Need 31 arguments")
 	}else{
-		return -3, nil
+		return error, nil
 	}
 		
 	
